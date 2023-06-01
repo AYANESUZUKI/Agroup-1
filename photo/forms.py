8027b4886d22da27b9478f96498c634d002051d2
鈴木彩音 <ayane_suzuki@totec.co.jp>
2023-06-01 16:08:07 +0900

    フォームクラス作成　コメント追加

--- photo/forms.py
+++ photo/forms.py
@@ -2,8 +2,14 @@ from django.forms import ModelForm
 from .models import PhotoPost
 
 class PhotoPostForm(ModelForm):
+   '''ModelFormのサブクラス
+   '''
+   class Meta:
+        '''ModelFormのインナークラス
 
-    class Meta:
-
+        Attributes:
+         model:モデルのクラス
+         fields:フォームで使用するモデルのフィールドを指定
+        '''
         model = PhotoPost
         fields = ['category','title','comment','image1','image2']
